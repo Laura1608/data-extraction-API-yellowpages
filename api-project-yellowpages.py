7c6e56b22b19765b9357334044fb1fc5b557397e
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2023-12-22 12:40:41 +0100

    Fixed indentation in for loop

--- api-project-yellowpages.py
+++ api-project-yellowpages.py
@@ -3,47 +3,48 @@ import csv
 import os.path
 
 # Connect to Yellow Pages API category page
-url = 'https://api.yellowpageskenya.com/categories'
-payload = {"searchParameters": {"LINGUA_ID": "2", "country": "CBV"}}
+url_categories = 'https://api.yellowpageskenya.com/categories'
+payload_categories = {"searchParameters": {"LINGUA_ID": "2", "country": "CBV"}}
 
 # Retrieve data and change format
-response = requests.post(url, json=payload)
-data = response.json()
-results = data.get('categories')
+response_categories = requests.post(url_categories, json=payload_categories)
+data_categories = response_categories.json()
+results_categories = data_categories.get('categories')
 
 # Create empty list to append categories
 categories = []
 
 # Loop over dictionary (in list) with all categories
-for dictionary in results:
+for dictionary in results_categories:
     category = dictionary['category']
     categories.append(category)
 
 # Connect to Yellow Pages API business page
 # Change API request depending on category name
 for i in categories:
-    url = 'https://api.yellowpageskenya.com/v1/search'
-    payload = {"searchParameters": {"LINGUA_ID": "2", "PESQUISA_F": f'{i}', "LOCALITY_F": "", "country": "CBV"}}
+    url_business = 'https://api.yellowpageskenya.com/v1/search'
+    payload_business = {"searchParameters": {"LINGUA_ID": "2", "PESQUISA_F": f'{i}', "LOCALITY_F": "", "country": "CBV"}}
 
-# Retrieve data and change format
-    response = requests.post(url, json=payload)
-    data = response.json()
-    results = data.get('results')
+    # Retrieve data and change format
+    response_business = requests.post(url_business, json=payload_business)
+    data_business = response_business.json()
+    results_business = data_business.get('results')
 
-# Write code to see if file exists
+    # Write code to see if file exists
     file_exists = os.path.isfile("Paginas_Amarelas_Output.csv")
 
-# Create new csv file to add data to
+    # Create new csv file to add data to
     with open("Paginas_Amarelas_Output.csv", "a", newline='') as file:
-        headers = ["title", "bname", "locality", "region", "baddress", "bemail", "bphone", "bmobile", "burl"]
+        headers = ["nome_loc_tit_id", "title", "bname", "descricao", "baddress", "locality", "region", "map_lat", "map_long", "bphone", "bmobile", "bemail", "burl", "logo"]
+
         writer = csv.DictWriter(file, delimiter=',', fieldnames=headers, extrasaction='ignore', dialect='unix')
 
-# Only add headers when the file is newly created
+        # Only add headers when the file is newly created
         if not file_exists:
             writer.writeheader()
 
-# Loop over every result on business page
-        for item in results:
+        # Loop over every result on business page
+        for item in results_business:
             writer.writerow(item)
 
 print('Company information retrieved!')
